ba6e38f9fa14d2e13bd60902d5b378634451ec32
Billy Gaskin <56692223+bmgaskin@users.noreply.github.com>
2020-12-02 22:10:44 -0600

    Update BG-NATO Alphabet.py

--- BG-NATO Alphabet.py
+++ BG-NATO Alphabet.py
@@ -1,9 +1,7 @@
-################################################
-# NATO Phonetic Alphabet Guide                 #
-# Version: 1.0                                 #
-# Date: 24-NOV-2020                            #
-# Developed by: Billy Gaskin                   #
-################################################
+# NATO Phonetic Alphabet Guide                 
+# Version: 1.0                            
+# Date: 24-NOV-2020                   
+# Developed by: Billy Gaskin
 #
 # Reference: https://en.wikipedia.org/wiki/NATO_phonetic_alphabet
 
